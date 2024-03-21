8741eda2b828a40f20b0e5c89a0735ac1de8e550
DBDA d. <DBDA@dypm.org>
2024-03-21 21:53:38 +0530

    Numbers

--- Add_numbers.py
+++ Add_numbers.py
@@ -1,9 +1,9 @@
 
-num1 = 1.5
-num2 = 6.3
+number1 = 1.5
+number2 = 6.3
 
 # To Add numbers
-sum = num1 + num2
+sum = number1 + number2
 
 # Display the sum
-print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+print('The sum of {0} and {1} is {2}'.format(number1, number2, sum))
